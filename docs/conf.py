78c1ecd66953fc70e0ddac3c92247b090eeaca90
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2024-02-06 16:54:32 +0100

    Work please

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Path setup --------------------------------------------------------------
 
 project = "blentom"
 copyright = "2024, Dominik Brandstetter"
@@ -12,16 +12,16 @@ author = "Dominik Brandstetter"
 release = "0.1"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
 templates_path = ["source/_templates"]
 exclude_patterns = []
 
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
 html_static_path = ["source/_static"]
