08fd56abbf14a808aa3d997bc5ae48e607703bea
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2024-02-17 22:09:26 +0100

    more material changes

--- src/material.py
+++ src/material.py
@@ -1,3 +1,4 @@
+import logging
 from os.path import exists
 from pathlib import Path
 
@@ -13,13 +14,10 @@ class Material:
 
     def __init__(self, name):
         """
-        Initialize a Material object.
+        Get an existing Blender material object. If the material does not exist, tries to load it from a materials file. User file is preferred. If the material is not found in any file, creates a new material.
 
         Args:
             name (str): The name of the material.
-
-        Raises:
-            KeyError: If the material is not found.
         """
         if len(name) <= 2:
             try:
@@ -30,12 +28,32 @@ class Material:
         material = bpy.data.materials.get(name)
         if material is None:
             try:
-                material = Material.load(name)
-            except KeyError:
-                print("Material not found. Creating new material.")
-                material = Material.create(name)
+                Material._load(name)
+            except (ValueError, RuntimeError):
+                logging.warning(f"Material {name} not found. Creating a new material.")
+                Material._create(name)
 
-        self._material = material
+        self._material = bpy.data.materials.get(name)
+
+    @property
+    def name(self):
+        """
+        Get the name of the material.
+
+        Returns:
+            str: The name of the material.
+        """
+        return self._material.name
+
+    @name.setter
+    def name(self, name):
+        """
+        Set the name of the material.
+
+        Args:
+            name (str): The name of the material.
+        """
+        self._material.name = name
 
     @property
     def material(self):
@@ -58,54 +76,72 @@ class Material:
         self._material = material
 
     @classmethod
-    def load(cls, name):
-        """
-        Load a material from file.
-
-        Args:
-            name (str): The name of the material.
-
-        Returns:
-            Material: The loaded material.
-
-        Raises:
-            KeyError: If the material is not found.
-            RuntimeError: If the material file is not found.
-        """
-        try:
-            file = str(Material.materials_directory / f"materials_user.blend")
-            Material._load(file, name)
-            return Material(name)
-        except (KeyError, RuntimeError):
-            file = str(Material.materials_directory / f"materials.blend")
-            Material._load(file, name)
-            return Material(name)
-
-    @classmethod
-    def create(cls, name, properties={}):
-        """
-        Create a new material.
-
-        Args:
-            name (str): The name of the material.
-            properties (dict): Additional properties for the material.
-
-        Returns:
-            Material: The created material.
-        """
+    def _create(cls, name):
         material = bpy.data.materials.new(name=name)
         material.use_nodes = True
         material.use_backface_culling = True
         material.blend_method = "BLEND"
-        node = material.node_tree.nodes["Principled BSDF"]
+        material.node_tree.nodes["Principled BSDF"]
 
-        for key, value in properties.items():
+    @property
+    def properties(self):
+        """
+        Get the properties of the material.
+
+        Note:
+            Only possible if material has a Principled BSDF node.
+
+        Returns:
+            dict: The properties of the material.
+
+        Raises:
+            RuntimeError: Principled BSDF node is not found.
+        """
+        properties = {}
+        try:
+            shader = self._material.node_tree.nodes["Principled BSDF"]
+        except KeyError:
+            for node in self._material.node_tree.nodes:
+                if node.type == "BSDF_PRINCIPLED":
+                    shader = node
+                    break
+            if shader is None:
+                raise RuntimeError("Principled BSDF node not found")
+
+        for input in shader.inputs:
+            properties[input.name] = input.default_value
+
+        return properties
+
+    @properties.setter
+    def properties(self, properties):
+        """
+        Set the properties of the material.
+
+        Note:
+            Only possible to Principled BSDF node properties.
+
+        Args:
+            properties (dict): The properties to set.
+
+        Raises:
+            RuntimeError: Principled BSDF node is not found.
+        """
+        try:
+            shader = self._material.node_tree.nodes["Principled BSDF"]
+        except KeyError:
+            for node in self._material.node_tree.nodes:
+                if node.type == "BSDF_PRINCIPLED":
+                    shader = node
+                    break
+            if shader is None:
+                raise RuntimeError("Principled BSDF node not found")
+
+        for property, value in properties.items():
             try:
-                node.inputs[key].default_value = value
+                shader.inputs[property].default_value = value
             except KeyError:
-                pass
-
-        return Material(name)
+                raise KeyError(f"Property {property} not found")
 
     def edit(self, property, value):
         """
@@ -131,30 +167,41 @@ class Material:
             raise KeyError(f"Property {property} not found.")
 
     @classmethod
-    def _load(cls, file, name):
+    def _load(cls, name):
         """
-        Load a material from file.
+        Load a material from material files. User file is preferred.
 
         Args:
-            file (str): The path to the material file.
-            name (str): The name of the material.
+            name (str): The name of the material to load.
 
         Raises:
-            KeyError: If the material is not found.
             RuntimeError: If the material file is not found.
+            ValueError: If the specified material is not found in the file.
         """
-        if not exists(file):
-            raise RuntimeError(f"{file} not found")
+
+        def _load_from_file(file, name):
+            if not exists(file):
+                raise RuntimeError(f"{file} not found")
+
+            try:
+                bpy.ops.wm.append(
+                    filepath="/Material/" + name,
+                    filename=name,
+                    directory=file + "/Material/",
+                )
+            except RuntimeError:
+                raise RuntimeError(f"Materials file {file} not found.")
+
+            material = bpy.data.materials.get(name)
+            if material is None:
+                raise ValueError(f"Material {name} not found.")
 
         try:
-            bpy.ops.wm.append(
-                filepath="/Material/" + name,
-                filename=name,
-                directory=file + "/Material/",
-            )
-        except RuntimeError:
-            raise KeyError(f"Material {name} not found")
-
-        material = bpy.data.materials.get(name)
-        if material is None:
-            raise KeyError(f"Material {name} not found")
+            file = str(Material.materials_directory / f"materials_user.blend")
+            _load_from_file(file, name)
+        except (ValueError, RuntimeError):
+            try:
+                file = str(Material.materials_directory / f"materials.blend")
+                _load_from_file(file, name)
+            except (ValueError, RuntimeError):
+                raise ValueError(f"Material file not found.")
